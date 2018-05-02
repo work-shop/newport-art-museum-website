c3f9911f409d73e5f6412cfccb1a9aaca2825e58
Greg Nemes <mail@gregnemes.com>
2018-05-02 16:09:25 -0400

    fixed problem with flexible content background data model

--- wp-content/themes/custom/functions/library/class-ws-flexible-content.php
+++ wp-content/themes/custom/functions/library/class-ws-flexible-content.php
@@ -2,35 +2,34 @@
 
 class WS_Flexible_Content_Helper{
 
-	public static function fc_background_text($heading, $text_alignment, $text_color, $subheading, $link_url, $link_text){ ?>
+	public static function fc_background_text( $background_type, $heading, $text_alignment, $text_color, $subheading, $link_url, $link_text){ ?>
 		<?php if( $heading ): ?>
-			<div class="fc-background-heading <?php echo $text_alignment; ?>">
+			<div class="fc-background-heading <?php if( $background_type !== 'multi-column' ): echo $text_alignment; endif; ?>">
 				<h2 class="serif <?php echo $text_color . ' '; echo $text_alignment; ?>"><?php echo $heading; ?></h2>
 			</div>
 		<?php endif; ?>
 		<?php if( $heading ): ?>
-			<div class="fc-background-subheading <?php echo $text_alignment; ?>">
+			<div class="fc-background-subheading <?php if( $background_type !== 'multi-column' ): echo $text_alignment; endif; ?>">
 				<h3 class="serif <?php echo $text_color . ' '; echo $text_alignment; ?>"><?php echo $subheading; ?></h3>
 			</div>
 		<?php endif; ?>
 		<?php if( $link_url ): ?>
-			<div class="fc-background-link fc-button <?php echo $text_alignment; ?>">
+			<div class="fc-background-link fc-button <?php if( $background_type !== 'multi-column' ): echo $text_alignment; endif; ?>">
 				<a href="<?php echo $link_url; ?>" class="<?php echo $text_color; ?>"><?php echo $link_text; ?></a>
 			</div>
 		<?php endif; ?>
 	<?php } 
 
-	public static function fc_background_image( $background_image, $section_height_image, $background_image_masking ){ ?>
-		<?php if( $background_image && $section_height_image !== 'natural-image' ): ?>
-			<div class="block-background <?php echo $background_image_masking; ?>" style="background-image: url('<?php echo $background_image; ?>');">
+	public static function fc_background_image( $background_type, $background_image, $section_height_image, $background_image_masking ){ ?>
+		<?php if( ($background_image && $section_height_image !== 'natural-image') || $background_type === 'multi-column' ): ?>
+			<div class="block-background <?php if( $background_type !== 'multi-column' ): echo $background_image_masking; endif; ?>" style="background-image: url('<?php echo $background_image; ?>');">
 			</div>
-		<?php elseif( $background_image && $section_height_image === 'natural-image' ): ?>
+		<?php elseif( $background_type === 'image' && $background_image && $section_height_image === 'natural-image' ): ?>
 			<div class="fc-background-natural-image-container <?php echo $background_image_masking; ?>">
 				<img src="<?php echo $background_image; ?>">
 			</div>
 		<?php endif; ?>		
 	<?php }
-
 
 }
 ?>
--- wp-content/themes/custom/partials/flexible_content/background.php
+++ wp-content/themes/custom/partials/flexible_content/background.php
@@ -32,34 +32,34 @@ endif; ?>
 
 <section class="block flexible-content fc fc-background <?php echo $fc_background_classes; ?>">
 	<?php if( $background_type === 'image' ): ?>
-		<?php WS_Flexible_Content_Helper::fc_background_image( $background_image, $section_height_image, $background_image_masking ); ?>
+		<?php WS_Flexible_Content_Helper::fc_background_image( $background_type, $background_image, $section_height_image, $background_image_masking ); ?>
 		<?php if( $include_text ): ?>
 			<div class="container-fc fc-background-text">
 				<div class="row">
 					<div class="col-sm-12">
-						<?php WS_Flexible_Content_Helper::fc_background_text($heading, $text_alignment, $text_color, $subheading, $link_url, $link_text); ?>
+						<?php WS_Flexible_Content_Helper::fc_background_text( $background_type, $heading, $text_alignment, $text_color, $subheading, $link_url, $link_text); ?>
 					</div>
 				</div>
 			</div>
 		<?php endif; ?>
 
 	<?php elseif( $background_type === 'multi-column'): ?>
-		<div class="fc-background-multi-column-container <?php // echo 'vhmin' . $section_height_image; ?> <?php echo $multi_column_layout; ?> <?php echo $image_width; ?>">
-			<div class="fc-background-multi-column-1 <?php // echo 'vhmin' . $section_height_image; ?> <?php if( $multi_column_layout === 'image-right' ): echo $background_color; ?> <?php endif; ?>">
+		<div class="fc-background-multi-column-container <?php echo $multi_column_layout; ?> <?php echo $image_width; ?>">
+			<div class="fc-background-multi-column-1 <?php if( $multi_column_layout === 'image-right' ): echo $background_color; ?> <?php endif; ?>">
 				<div class="fc-background-multi-column-1-inner">
 					<?php if( $multi_column_layout === 'image-left' ): ?>
-						<?php WS_Flexible_Content_Helper::fc_background_image( $background_image, $section_height_image, $background_image_masking ); ?>
+						<?php WS_Flexible_Content_Helper::fc_background_image( $background_type, $background_image, $section_height_image, $background_image_masking ); ?>
 					<?php else: ?>
-						<?php WS_Flexible_Content_Helper::fc_background_text($heading, $text_alignment, $text_color, $subheading, $link_url, $link_text); ?>
+						<?php WS_Flexible_Content_Helper::fc_background_text( $background_type, $heading, $text_alignment, $text_color, $subheading, $link_url, $link_text); ?>
 					<?php endif; ?>
 				</div>
 			</div>
-			<div class="fc-background-multi-column-2 <?php // echo 'vhmin' . $section_height_image; ?> <?php if( $multi_column_layout === 'image-left' ): echo $background_color; ?> <?php endif; ?>">
+			<div class="fc-background-multi-column-2 <?php if( $multi_column_layout === 'image-left' ): echo $background_color; ?> <?php endif; ?>">
 				<div class="fc-background-multi-column-2-inner">
 					<?php if( $multi_column_layout === 'image-right' ): ?>
-						<?php WS_Flexible_Content_Helper::fc_background_image( $background_image, $section_height_image, $background_image_masking ); ?>
+						<?php WS_Flexible_Content_Helper::fc_background_image( $background_type, $background_image, $section_height_image, $background_image_masking ); ?>
 					<?php else: ?>
-						<?php WS_Flexible_Content_Helper::fc_background_text($heading, $text_alignment, $text_color, $subheading, $link_url, $link_text); ?>
+						<?php WS_Flexible_Content_Helper::fc_background_text( $background_type, $heading, $text_alignment, $text_color, $subheading, $link_url, $link_text); ?>
 					<?php endif; ?>
 				</div>
 			</div>
@@ -70,7 +70,7 @@ endif; ?>
 			<div class="container-fc fc-background-text">
 				<div class="row">
 					<div class="col-sm-12">
-						<?php WS_Flexible_Content_Helper::fc_background_text($heading, $text_alignment, $text_color, $subheading, $link_url, $link_text); ?>
+						<?php WS_Flexible_Content_Helper::fc_background_text( $background_type, $heading, $text_alignment, $text_color, $subheading, $link_url, $link_text); ?>
 					</div>
 				</div>
 			</div>
