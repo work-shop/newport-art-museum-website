4e16e163acfc2a23a4b41806cdb4ba804fe20376
Greg Nemes <mail@gregnemes.com>
2018-05-15 09:36:25 -0400

    slight adjustment to image sizes

--- wp-content/themes/custom/functions/class-nam-site-init.php
+++ wp-content/themes/custom/functions/class-nam-site-init.php
@@ -44,10 +44,9 @@ class NAM_Site {
             add_image_size('acf_preview', 300, 300, false);
             add_image_size('page_hero', 1440, 660, false);
             add_image_size('home_hero', 1920, 1200, false);
-            add_image_size('card_large', 1162, 538, true);
             add_image_size('card_wide', 1162, 538, true);
             add_image_size('card_medium', 500, 375, true);
-            
+
         }
     }
 
