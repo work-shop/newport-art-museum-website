74eff463d3273fb8b8ff795162259d252bec064e
Greg Nemes <mail@gregnemes.com>
2018-06-04 18:44:38 -0400

    improved plan your visit section

--- wp-content/themes/custom/partials/visit/hours.php
+++ wp-content/themes/custom/partials/visit/hours.php
@@ -1,12 +1,12 @@
 <?php if( have_rows('hours_content','23') ): ?>
 	<?php while ( have_rows('hours_content','23') ) : the_row(); ?>
 		<div class="row hours">
-			<div class="col-md-5 col-xs-5">
+			<div class="col-md-5 col-5">
 				<h4 class="plan-your-visit-days key">
 					<?php the_sub_field('days'); ?>
 				</h4>
 			</div>
-			<div class="col-md-7 col-xs-7">
+			<div class="col-md-7 col-7">
 				<h4 class="plan-your-visit-hours value">
 					<?php the_sub_field('hours'); ?>
 				</h4>
