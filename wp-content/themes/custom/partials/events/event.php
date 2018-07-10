0e658fa9b9e975cc8cd055d8b5d4cad6067c52a9
Greg Nemes <mail@gregnemes.com>
2018-07-10 17:32:34 -0400

    completed first draft of event single

--- wp-content/themes/custom/partials/events/event.php
+++ wp-content/themes/custom/partials/events/event.php
@@ -31,17 +31,11 @@ $is_on_sale = $product->is_on_sale();
 $is_in_stock = $product->is_in_stock();
 $stock_quantity = $product->get_stock_quantity();
 
-// custom fields
-$show_hero = get_field('show_hero');
-$hero_image = get_field('hero_image');
-$hero_text = get_field('hero_text');
-
 $event_date = get_field('event_date');
-$event_time = get_field('event_time');
+$event_start_time = get_field('event_start_time');
+$event_end_time = get_field('event_end_time');
 $event_location = get_field('event_location');
 $event_short_description = get_field('short_description');
-
-$flexible_content = get_field('flexible_content_sections');
 
 // woocommerce plumbing
 $add_to_cart_url = $product->add_to_cart_url();
@@ -50,57 +44,97 @@ $add_to_cart_url = $product->add_to_cart_url();
 
 ?>
 
-<section class="container-fluid">
-    <div class="row mt2 mb2">
-        <div class="col-md-6">
-            <div class="mb2">
-                <?php nam_debug_formatting('h3', 'Name', $name ); ?>
-            </div>
-            <div class="mb2">
-                <?php nam_debug_formatting('h5', 'Current Price', $current_price ); ?>
-                <?php nam_debug_formatting('h5', 'Sale Price', $sale_price ); ?>
-                <?php nam_debug_formatting('h5', 'Regular Price', $regular_price ); ?>
-                <?php nam_debug_formatting('h5', 'Sale Start Date', $sale_start_date ); ?>
-                <?php nam_debug_formatting('h5', 'Sale End Date', $sale_end_date ); ?>
-                <?php nam_debug_formatting('h5', 'On Sale?', $is_on_sale ); ?>
-            </div>
-            <div class="mb2">
-                <?php nam_debug_formatting('h5', 'In Stock?', $is_in_stock ); ?>
-                <?php nam_debug_formatting('h5', 'Stock Quantity', $stock_quantity ); ?>
-            </div>
-        </div>
+<section class="block single-body" id="event-single-body">
+    <div class="container-fluid-single container-fluid">
+        <div class="row">
+            <div class="col-md-8 single-body-left">
+                <div class="single-body-left-link">
+                    <a href="/events">Back To Events</a>
+                </div>
+                <div class="single-body-left-main">
+                    <div class="event-single-introduction single-introduction">
+                        <h1 class="serif event-single-title single-title">
+                            <?php the_title(); ?>
+                        </h1>
+                        <h3 class="event-single-short-description mt1">
+                            <?php echo $event_short_description; ?>
+                        </h3>
+                        <h4 class="event-single-info">
+                            <span class="event-date mr2"><?php echo $event_date; ?></span>
+                            <span class="event-time mr2"><?php echo $event_start_time; ?><?php if($event_end_time): echo ' - ' . $event_end_time; endif; ?></span>
+                            <span class="event-location"><?php echo $event_location; ?></span>
+                        </h4>
+                        <div class="row">
+                            <div class="col">
+                                <div class="nam-dash nam-dash-desktop"></div>
+                                <div class="sidebar single-sidebar shadowed sidebar-responsive">
+                                    <div class="single-sidebar-top">
+                                        <h4 class="bold sidebar-heading">
+                                            <?php if( get_field('registration_required') ): ?>
+                                                <?php if ( $is_in_stock ) : ?>
+                                                    Tickets
+                                                    <?php if ( !$current_price > 0 ): ?>
+                                                        <br>
+                                                        <span class="free-registration-text h5 mt1">
+                                                            This Event is Free, but Requires Registration. 
+                                                        </span>
+                                                    <?php endif; ?>
+                                                    <?php else: ?>
+                                                        <span class="error bold m1 display-block">This Event is Sold Out.</span>
+                                                    <?php endif; ?>
+                                                    <?php else: ?>
+                                                        <span class="bold display-block m1">
+                                                            This Event is Free and Open to the Public, No Registration or Tickets Required.
+                                                        </span>
+                                                    <?php endif; ?>
+                                                </h4>
+                                            </div>
+                                            <?php if ( $is_in_stock ) : ?>
+                                                <?php include( locate_template('partials/ecommerce/single_add_to_cart.php') ); ?>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-body-left-content">
+                                <?php get_template_part('partials/flexible_content/flexible_content'); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 single-body-right single-body-right-desktop">
+                        <div class="single-body-right-content">
+                            <div class="sidebar single-sidebar shadowed">
+                                <div class="single-sidebar-top">
+                                    <h4 class="bold sidebar-heading">
+                                        <?php if( get_field('registration_required') ): ?>
+                                            <?php if ( $is_in_stock ) : ?>
+                                                Tickets
+                                                <?php if ( !$current_price > 0 ): ?>
+                                                    <br>
+                                                    <span class="free-registration-text h5 mt1">
+                                                        This Event is Free, but Requires Registration. 
+                                                    </span>
+                                                <?php endif; ?>
+                                                <?php else: ?>
+                                                    <span class="error bold m1 display-block">This Event is Sold Out.</span>
+                                                <?php endif; ?>
+                                                <?php else: ?>
+                                                    <span class="bold display-block m1">
+                                                        This Event is Free and Open to the Public, No Registration or Tickets Required.
+                                                    </span>
+                                                <?php endif; ?>
+                                            </h4>
+                                        </div>
+                                        <?php if ( $is_in_stock ) : ?>
+                                            <?php include( locate_template('partials/ecommerce/single_add_to_cart.php') ); ?>
+                                        <?php endif; ?>
+                                    </div><!-- .sidebar -->
+                                </div><!-- .single-body-right-content -->
+                            </div><!-- .single-body-right -->
+                        </div><!-- .container-fluid -->
+                    </div><!-- .row-->
+                </section>
 
-        <div class="col-md-6">
-            <div class="mb2">
-                <p class="mb1">Page Hero Metadata</p>
-                <?php nam_debug_formatting( 'h6', 'Show Hero?', $show_hero); ?>
-                <?php nam_debug_formatting( 'h6', 'Hero Image', $hero_image['url'] ); ?>
-                <?php nam_debug_formatting( 'h6', 'Show Text', $hero_text ); ?>
-            </div>
-
-            <div class="mb2">
-                <p class="mb1">Event Details</p>
-                <?php nam_debug_formatting( 'h6', 'Event Date', $event_date ); ?>
-                <?php nam_debug_formatting( 'h6', 'Event Time',$event_time ); ?>
-                <?php nam_debug_formatting( 'h6', 'Event Location', $event_location['address'] ); ?>
-                <?php nam_debug_formatting( 'h6', 'Event Short Description', $event_short_description ); ?>
-            </div>
-        </div>
-    </div>
 
 
-    <?php if ( $is_in_stock ) : ?>
 
-    <div class="row">
-        <div class="col-md-6">
-
-            <p class="mb1">Add to Cart Form</p>
-
-            <?php include( locate_template('partials/ecommerce/add_to_cart.php') ); ?>
-
-        </div>
-    </div>
-
-    <?php endif; ?>
-
-</section>
--- wp-content/themes/custom/single-events.php
+++ wp-content/themes/custom/single-events.php
@@ -1,6 +1,8 @@
 
 <?php get_template_part('partials/header'); ?>
 
+<?php get_template_part('partials/page/page_hero' ); ?>
+
 <?php get_template_part('partials/events/event' ); ?>
 
 <?php get_template_part('partials/footer' ); ?>
