df6fb8da16f6a519a42ccc8990486130790c9314
Greg Nemes <mail@gregnemes.com>
2018-10-23 14:09:03 -0400

    added deploy production script

---
+++ .deploy-production.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ./.env
+
+# Theme Uploads is on the following line
+scp -P $KINSTA_PRODUCTION_PORT -r ./wp-content/themes/custom $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes
