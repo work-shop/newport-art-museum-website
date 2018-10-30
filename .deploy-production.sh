f9cc094a9368dd61af55aaef614b148382233003
Greg Nemes <mail@gregnemes.com>
2018-10-30 10:34:50 -0400

    changed deploy production script

--- .deploy-production.sh
+++ .deploy-production.sh
@@ -3,4 +3,5 @@
 source ./.env
 
 # Theme Uploads is on the following line
-scp -P $KINSTA_PRODUCTION_PORT -r ./wp-content/themes/custom $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes
+#scp -P $KINSTA_PRODUCTION_PORT -r ./wp-content/themes/custom $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes
+scp -P $KINSTA_PRODUCTION_PORT -r ./wp-content/plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content
