a38f4f6b5d242a47ac5dda6f986e0b52ce2e6ab8
Nic Schumann <nicolas_schumann@brown.edu>
2018-11-05 16:15:53 -0500

    [classes] reworked e-commerce sidebar to respond to different class-in-cart states.

--- wp-content/themes/custom/partials/ecommerce/single_add_to_cart.php
+++ wp-content/themes/custom/partials/ecommerce/single_add_to_cart.php
@@ -5,12 +5,13 @@
  */
 
 $class_in_cart = NAM_Classes::has_class_in_cart();
+$is_classes = get_post_type( get_the_ID() ) == 'classes';
 
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
 <div class="single-sidebar-add-to-cart">
 
-    <?php if ( $class_in_cart && get_post_type( get_the_ID() ) === 'classes' ) : // we're on a class page and there's a class in the cart. ?>
+    <?php if ( $class_in_cart && $is_classes ) : // we're on a class page and there's a class in the cart. ?>
 
         <div class="single-sidebar-middle">
             <div class="row">
@@ -38,7 +39,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
             do_action( 'woocommerce_before_add_to_cart_quantity' ); ?>
             <div class="single-sidebar-middle">
                 <div class="row">
-                    <div class="col-6 add-to-cart-price">
+                    <div class="col-<?php if ( $is_classes ) : ?>12<?php else: ?>6<?php endif; ?> add-to-cart-price">
                         <h5 class="label bold">
                             Price
                         </h5>
@@ -49,12 +50,12 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
                            <?php if ( $is_member_or_has_membership_in_cart && $current_price > 0 ) : ?>
 
                                <p class="members-price"><?php echo wc_price($current_price - $membership_discount); ?> Per Person</p>
-                               <p class="non-members-price">(<?php echo wc_price( $current_price ); ?> For Non-members)</p>
+                               <?php if ( $membership_discount > 0 ) : ?><p class="non-members-price">(<?php echo wc_price( $current_price ); ?> For Non-members)</p><?php endif; ?>
 
                             <?php elseif ( $current_price > 0 ): ?>
 
                                <p class="non-members-price"><?php echo wc_price( $current_price ); ?> Per Person</p>
-                               <p class="non-members-price">(<?php echo wc_price( $current_price - $membership_discount ); ?> For Members!)</p>
+                              <?php if ( $membership_discount > 0 ) : ?><p class="non-members-price">(<?php echo wc_price( $current_price - $membership_discount ); ?> For Members!)</p><?php endif; ?>
 
                             <?php else: ?>
 
@@ -63,19 +64,43 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
                             <?php endif; ?>
                         </h5>
                     </div>
-                    <div class="col-6 add-to-cart-quantity">
-                        <h5 class="label bold righted">
-                            Quantity
-                        </h5>
-                        <?php
-                        woocommerce_quantity_input( array(
-                            'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-                            'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-                            'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : $product->get_min_purchase_quantity(),
-                        ) );
-                        ?>
-                    </div>
-                    <?php do_action( 'woocommerce_after_add_to_cart_quantity' ); ?>
+
+                    <?php if ( !$is_classes ) : // if it's not classes, add the quantity button. ?>
+                        <div class="col-6 add-to-cart-quantity">
+                            <h5 class="label bold righted">
+                                Quantity
+                            </h5>
+                            <?php
+                            woocommerce_quantity_input( array(
+                                'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+                                'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+                                'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : $product->get_min_purchase_quantity(),
+                            ) );
+                            ?>
+                        </div>
+                        <?php do_action( 'woocommerce_after_add_to_cart_quantity' ); ?>
+                    <?php endif; ?>
+
+                    <?php if ( $product instanceof WC_Product_Bundle ) : // if it's a bundle with fees, render the fees in their own section. ?>
+                        <?php $bundled_items = $product->get_bundled_data_items(); ?>
+                        <?php if ( count( $bundled_items ) > 0 ) : ?>
+
+                            <div class="col-12 add-to-cart-fees">
+                                <h5 class="label bold">
+                                    Fees
+                                </h5>
+                                <?php foreach( $bundled_items as $fee ) : ?>
+
+                                    <?php $fee_product = wc_get_product( $fee->get_product_id() ); ?>
+
+                                    <p class="small"><?php echo wc_price( $fee_product->get_price() ); ?> <?php echo $fee_product->name; ?> Per Person</p>
+
+                                <?php endforeach; ?>
+                            </div>
+
+                        <?php endif; ?>
+                    <?php endif; ?>
+
                 </div>
             </div>
             <button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button-full button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
