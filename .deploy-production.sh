e278b7dbeb65cd0780ad1148367db73b1395ab04
Greg Nemes <mail@gregnemes.com>
2018-11-09 08:24:44 -0500

    added cache clear to deploy production script

--- .deploy-production.sh
+++ .deploy-production.sh
@@ -9,3 +9,5 @@ scp -P $KINSTA_PRODUCTION_PORT -r ./wp-content/themes/custom $KINSTA_PRODUCTION_
 #scp -P $KINSTA_PRODUCTION_PORT ./wp-content/themes/custom/functions/class-nam-site-admin.php $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes/custom/functions/
 
 #scp -P $KINSTA_PRODUCTION_PORT -r ./wp-content/plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content
+
+curl -L https://newportartmuseum.org/kinsta-clear-cache-all/ 
