cf7f6b970020c0640e3a6919acac2a93344b39a0
Greg Nemes <mail@gregnemes.com>
2018-11-20 11:14:23 -0500

    edited renewl invoice email template

--- wp-content/themes/custom/woocommerce/emails/customer-renewal-invoice.php
+++ wp-content/themes/custom/woocommerce/emails/customer-renewal-invoice.php
@@ -12,24 +12,64 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+<?php 
+$user = get_user_by('login',$user_login); 
+$first_name = $user->user_firstname; 
+$email_address = $user->user_email; 
+$user_id = $user->ID;
+$user_was_imported =  NAM_Membership::user_was_imported($user_id );
+?>
 
 <?php if ( 'pending' == $order->get_status() ) : ?>
 	<p>
+		Your Newport Art Museum Membership has expired.
 		<?php
 		// translators: %1$s: name of the blog, %2$s: link to checkout payment url, note: no full stop due to url at the end
-		echo wp_kses( sprintf( _x( 'Your Newport Art Museum membership has expired. To renew, please use the following link: %2$s', 'In customer renewal invoice email', 'woocommerce-subscriptions' ), esc_html( get_bloginfo( 'name' ) ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . esc_html__( 'Pay Now &raquo;', 'woocommerce-subscriptions' ) . '</a>' ), array( 'a' => array( 'href' => true ) ) );
+		//echo wp_kses( sprintf( _x( 'Your Newport Art Museum membership has expired. To renew, please use the following link: %2$s', 'In customer renewal invoice email', 'woocommerce-subscriptions' ), esc_html( get_bloginfo( 'name' ) ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . esc_html__( 'Renew Now &raquo;', 'woocommerce-subscriptions' ) . '</a>' ), array( 'a' => array( 'href' => true ) ) );
 		?>
 	</p>
+	<?php if( $user_was_imported ): ?>
+		<p>
+			<strong>
+			Please note: your account was automatically created from our membership database. To renew your membership, you will need to activate your account, if you have not already. To activate your account, follow these instructions:
+		</strong>
+	</p>
+	<p>
+	<?php if($email_address): ?>
+		Your account email address is <?php printf($email_address); ?><br>
+	<?php endif; ?>
+	<?php if($user_login): ?>
+		Your username is <?php printf($user_login); ?>
+	<?php endif; ?>
+</p>
+	<ol>
+		<li>Go to <a href="https://newportartmuseum.org/my-account/lost-password">https://newportartmuseum.org/my-account/lost-password</a></li> 
+		<li>Enter your email address</li>
+		<li>You will then receive an email from hello@newportartmuseum</li>
+		<li>Follow the link in that email to create a password</li>
+		<li>Log in </li>
+		<li>Once you are logged in, click on 'Memberships', click on your membership, then click on the button that says 'Renew Now'. 
+	</ol>
+	<br>
+		<p>
+		<?php
+		// translators: %1$s: name of the blog, %2$s: link to checkout payment url, note: no full stop due to url at the end
+		echo wp_kses( sprintf( _x( ' If you have already activated your account, %2$s', 'In customer renewal invoice email', 'woocommerce-subscriptions' ), esc_html( get_bloginfo( 'name' ) ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . esc_html__( 'Click Here to Renew Now.', 'woocommerce-subscriptions' ) . '</a>' ), array( 'a' => array( 'href' => true ) ) );
+		?>
+	</p>
+	<br>
+<?php endif; ?>
+
 <?php elseif ( 'failed' == $order->get_status() ) : ?>
 	<p>
 		<?php
 		// translators: %1$s: name of the blog, %2$s: link to checkout payment url, note: no full stop due to url at the end
 		echo wp_kses( sprintf( _x( 'The automatic payment to renew your subscription with %1$s has failed. To reactivate the subscription, please login and pay for the renewal from your account page: %2$s', 'In customer renewal invoice email', 'woocommerce-subscriptions' ), esc_html( get_bloginfo( 'name' ) ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . esc_html__( 'Pay Now &raquo;', 'woocommerce-subscriptions' ) . '</a>' ), array( 'a' => array( 'href' => true ) ) ); ?></p>
-<?php endif; ?>
+	<?php endif; ?>
 
-<?php do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email ); ?>
+	<?php do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email ); ?>
 
 	<p>If you have any questions, please <a href="https://newportartmuseum.org/contact" target="_blank">contact us.</a></p>
 	<p>Thank you.</p>	
 
-<?php do_action( 'woocommerce_email_footer', $email ); ?>
+	<?php do_action( 'woocommerce_email_footer', $email ); ?>
