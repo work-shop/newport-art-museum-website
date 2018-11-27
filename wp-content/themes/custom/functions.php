f32c26f4fb585a641e33d47d87f6d1ddd4ece7e3
Greg Nemes <mail@gregnemes.com>
2018-11-27 14:38:19 -0500

    turned off add to cart notice

--- wp-content/themes/custom/functions.php
+++ wp-content/themes/custom/functions.php
@@ -112,7 +112,7 @@ add_filter( 'woocommerce_email_headers', 'add_bcc_to_certain_emails', 10, 2 );
 
 
 //hide the 'product has been added to your cart notice'
-//add_filter( 'wc_add_to_cart_message_html', '__return_false' );
+add_filter( 'wc_add_to_cart_message_html', '__return_false' );
 
 
 //add capabilities to user roles
