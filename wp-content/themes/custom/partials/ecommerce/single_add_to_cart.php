15535e5ff80196c2bab16f0f251715a44ff442c6
Greg Nemes <mail@gregnemes.com>
2018-12-03 11:55:22 -0500

    added condition on checkout page for free products

--- wp-content/themes/custom/partials/ecommerce/single_add_to_cart.php
+++ wp-content/themes/custom/partials/ecommerce/single_add_to_cart.php
@@ -86,138 +86,142 @@ do_action('woocommerce_before_add_to_cart_form');
             ?>
             <?php //NO DISCOUNT ?>
             <?php if( $membership_discount == 0 ): ?>
-            <div class="row">
-              <div class="col">
-                <p class="no-discount-price m0">
-                  <?php echo wc_price($current_price); ?> Per person
-                </p>
-              </div>
-            </div>                
-          <?php else: //membership discount exists on product ?>
-            <?php //MEMBER PRICE ?>
-            <?php if ($is_member_or_has_membership_in_cart && $current_price > 0): ?>
               <div class="row">
-                <div class="class-price-col-first">
-                  <span class="icon" data-icon="%"></span>
-                </div>
-                <div class="class-price-col-second">
-                  <h5 class="bold mb0 price-discount-label">
-                    <span class="icon member-check-mark" data-icon="%"></span>Member price
-                  </h5>
-                  <p class="members-price m0">
-                    <?php echo wc_price($current_price - $membership_discount); ?> Per person
-                  </p>
-                  <?php if( $is_events ): ?>
-                    <p class="mt1 mb0 price-discount-label">
-                      <span class="faded bold">Your membership entitles you to<br>
-                        <?php echo $membership_discount_multiplier; ?> discounted ticket<?php if($membership_discount_multiplier > 1):?>s<?php endif; ?></span>
-                      </p>
-                    <?php endif; ?>
+                <div class="col">
+                  <p class="no-discount-price m0">
+                    <?php if ($current_price > 0): ?>
+                      <?php echo wc_price($current_price); ?> Per person
+                      <?php else: ?>
+                        Free
+                      <?php endif; ?>
+                    </p>
                   </div>
-                </div>
-                <?php //NON MEMBER PRICE ?>
-                <?php elseif ($current_price > 0): ?>
-                  <div class="row class-price-row mb1">
+                </div>                
+              <?php else: //membership discount exists on product ?>
+                <?php //MEMBER PRICE ?>
+                <?php if ($is_member_or_has_membership_in_cart && $current_price > 0): ?>
+                  <div class="row">
                     <div class="class-price-col-first">
                       <span class="icon" data-icon="%"></span>
                     </div>
                     <div class="class-price-col-second">
-                      <h5 class="bold m0 price-discount-label">
-                        Non-member price
+                      <h5 class="bold mb0 price-discount-label">
+                        <span class="icon member-check-mark" data-icon="%"></span>Member price
                       </h5>
-                      <p class="non-members-price m0">
-                        <?php echo wc_price($current_price); ?> Per person
+                      <p class="members-price m0">
+                        <?php echo wc_price($current_price - $membership_discount); ?> Per person
                       </p>
+                      <?php if( $is_events ): ?>
+                        <p class="mt1 mb0 price-discount-label">
+                          <span class="faded bold">Your membership entitles you to<br>
+                            <?php echo $membership_discount_multiplier; ?> discounted ticket<?php if($membership_discount_multiplier > 1):?>s<?php endif; ?></span>
+                          </p>
+                        <?php endif; ?>
+                      </div>
                     </div>
+                    <?php //NON MEMBER PRICE ?>
+                    <?php elseif ($current_price > 0): ?>
+                      <div class="row class-price-row mb1">
+                        <div class="class-price-col-first">
+                          <span class="icon" data-icon="%"></span>
+                        </div>
+                        <div class="class-price-col-second">
+                          <h5 class="bold m0 price-discount-label">
+                            Non-member price
+                          </h5>
+                          <p class="non-members-price m0">
+                            <?php echo wc_price($current_price); ?> Per person
+                          </p>
+                        </div>
+                      </div>
+                      <?php else: ?>
+                        <p>
+                          Free
+                        </p>
+                      <?php endif; //member or non member price?>
+                    <?php endif; //discount or no discount ?>
+                  </div><!-- .price-->
+                </div><!-- .col -->
+                <?php if ($is_events): ?>
+                  <div class="col add-to-cart-quantity">
+                    <?php
+                    woocommerce_quantity_input(array(
+                      'min_value' => 0,
+                      'max_value' => apply_filters('woocommerce_quantity_input_max', $purchase_option['max_qty'], $product),
+                      'input_value' => isset($_POST['quantity']) ? wc_stock_amount($_POST['quantity']) : (($i > 0) ? 0 : $product->get_min_purchase_quantity()),
+                    ));
+                    ?>
                   </div>
+                  <input type="hidden" name="variation_id" value="<?php echo $purchase_option['id'] ?>" />
+                  <input type="hidden" name="attribute_ticket_levels" value="<?php echo $purchase_option['term']->slug; ?>">
                   <?php else: ?>
-                    <p>
-                      Free
+                    <div class="col add-to-cart-quantity">
+                      <?php
+                      woocommerce_quantity_input(array(
+                        'min_value' => apply_filters('woocommerce_quantity_input_min', $purchase_option->get_min_purchase_quantity(), $purchase_option),
+                        'max_value' => apply_filters('woocommerce_quantity_input_max', $purchase_option->get_max_purchase_quantity(), $purchase_option),
+                        'input_value' => isset($_POST['quantity']) ? wc_stock_amount($_POST['quantity']) : $purchase_option->get_min_purchase_quantity(),
+                      ));
+                      ?>
+                    </div>
+                  <?php endif;?>
+                  <?php do_action('woocommerce_after_add_to_cart_quantity');?>
+                  <?php //moved member price row to here ?>
+                  <?php if (!$is_member_or_has_membership_in_cart && $current_price > 0 && $membership_discount > 0): ?>
+                    <div class="col-12 member-price-col">
+                      <a class="h5 bold price-discount-label modal-toggle" href="#" id="member-price-info" data-modal-target="modal-member-price-info">
+                        <span class="faded bold">Member price <?php echo wc_price($current_price - $membership_discount); ?> Per person</span><span class="icon" data-icon="?"></span>
+                      </a>
+                    </div>
+                  <?php endif; ?>
+                </div><!-- .row.add-to-cart-price -->
+                <?php if ($product instanceof WC_Product_Bundle): // if it's a bundle with fees, render the fees in their own section. ?>
+                 <?php $bundled_items = $product->get_bundled_data_items();?>
+                 <?php if (count($bundled_items) > 0): ?>
+                   <div class="row add-to-cart-fees">
+                     <div class="col-12">
+                       <h5 class="add-to-cart-label bold uppercase tracked-less">
+                         Fees
+                       </h5>
+                       <?php foreach ($bundled_items as $fee): ?>
+                         <?php $fee_product = wc_get_product($fee->get_product_id());?>
+                         <p class="mb0">
+                           <?php echo wc_price($fee_product->get_price()); ?> <?php echo $fee_product->name; ?> Per person
+                         </p>
+                       <?php endforeach; ?>
+                     </div>
+                   </div>
+                 <?php endif; ?>
+               <?php endif; ?>
+               <?php if ($is_classes): ?>
+                <div class="row add-to-cart-limit">
+                  <div class="col-12">
+                    <h5 class="add-to-cart-label">
+                      Registration Limit
+                    </h5>
+                    <p class="small mb0">
+                      <span class="icon icon-alert" data-icon="="></span>
+                      <?php the_field('class_registration_limit_explanation', 'option');?>
                     </p>
-                  <?php endif; //member or non member price?>
-                <?php endif; //discount or no discount ?>
-              </div><!-- .price-->
-            </div><!-- .col -->
-            <?php if ($is_events): ?>
-              <div class="col add-to-cart-quantity">
-                <?php
-                woocommerce_quantity_input(array(
-                  'min_value' => 0,
-                  'max_value' => apply_filters('woocommerce_quantity_input_max', $purchase_option['max_qty'], $product),
-                  'input_value' => isset($_POST['quantity']) ? wc_stock_amount($_POST['quantity']) : (($i > 0) ? 0 : $product->get_min_purchase_quantity()),
-                ));
-                ?>
-              </div>
-              <input type="hidden" name="variation_id" value="<?php echo $purchase_option['id'] ?>" />
-              <input type="hidden" name="attribute_ticket_levels" value="<?php echo $purchase_option['term']->slug; ?>">
-              <?php else: ?>
-                <div class="col add-to-cart-quantity">
-                  <?php
-                  woocommerce_quantity_input(array(
-                    'min_value' => apply_filters('woocommerce_quantity_input_min', $purchase_option->get_min_purchase_quantity(), $purchase_option),
-                    'max_value' => apply_filters('woocommerce_quantity_input_max', $purchase_option->get_max_purchase_quantity(), $purchase_option),
-                    'input_value' => isset($_POST['quantity']) ? wc_stock_amount($_POST['quantity']) : $purchase_option->get_min_purchase_quantity(),
-                  ));
-                  ?>
+                  </div>
                 </div>
               <?php endif;?>
-              <?php do_action('woocommerce_after_add_to_cart_quantity');?>
-              <?php //moved member price row to here ?>
-              <?php if (!$is_member_or_has_membership_in_cart && $current_price > 0 && $membership_discount > 0): ?>
-                <div class="col-12 member-price-col">
-                  <a class="h5 bold price-discount-label modal-toggle" href="#" id="member-price-info" data-modal-target="modal-member-price-info">
-                    <span class="faded bold">Member price <?php echo wc_price($current_price - $membership_discount); ?> Per person</span><span class="icon" data-icon="?"></span>
-                  </a>
-                </div>
-              <?php endif; ?>
-            </div><!-- .row.add-to-cart-price -->
-            <?php if ($product instanceof WC_Product_Bundle): // if it's a bundle with fees, render the fees in their own section. ?>
-             <?php $bundled_items = $product->get_bundled_data_items();?>
-             <?php if (count($bundled_items) > 0): ?>
-               <div class="row add-to-cart-fees">
-                 <div class="col-12">
-                   <h5 class="add-to-cart-label bold uppercase tracked-less">
-                     Fees
-                   </h5>
-                   <?php foreach ($bundled_items as $fee): ?>
-                     <?php $fee_product = wc_get_product($fee->get_product_id());?>
-                     <p class="mb0">
-                       <?php echo wc_price($fee_product->get_price()); ?> <?php echo $fee_product->name; ?> Per person
-                     </p>
-                   <?php endforeach; ?>
-                 </div>
-               </div>
-             <?php endif; ?>
-           <?php endif; ?>
-           <?php if ($is_classes): ?>
-            <div class="row add-to-cart-limit">
-              <div class="col-12">
-                <h5 class="add-to-cart-label">
-                  Registration Limit
-                </h5>
-                <p class="small mb0">
-                  <span class="icon icon-alert" data-icon="="></span>
-                  <?php the_field('class_registration_limit_explanation', 'option');?>
-                </p>
-              </div>
-            </div>
-          <?php endif;?>
-        </div><!-- .single-sidebar-middle -->
-        <input type="hidden" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" />
-        <?php if (!$is_events): ?>
-          <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button-full button alt">
+            </div><!-- .single-sidebar-middle -->
+            <input type="hidden" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" />
+            <?php if (!$is_events): ?>
+              <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button-full button alt">
+                <?php echo esc_html($product->single_add_to_cart_text()); ?>
+              </button>
+            <?php endif;?>
+            <?php do_action('woocommerce_after_add_to_cart_button');?>
+          </form>
+        <?php } ?>
+        <?php if ($is_events): ?>
+          <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="events_add_to_cart_button single_add_to_cart_button button-full button alt">
             <?php echo esc_html($product->single_add_to_cart_text()); ?>
           </button>
         <?php endif;?>
-        <?php do_action('woocommerce_after_add_to_cart_button');?>
-      </form>
-    <?php } ?>
-    <?php if ($is_events): ?>
-      <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="events_add_to_cart_button single_add_to_cart_button button-full button alt">
-        <?php echo esc_html($product->single_add_to_cart_text()); ?>
-      </button>
-    <?php endif;?>
 
 
-  <?php endif;?>
-  <?php do_action('woocommerce_after_add_to_cart_form');?>
+      <?php endif;?>
+      <?php do_action('woocommerce_after_add_to_cart_form');?>
--- wp-content/themes/custom/woocommerce/checkout/form-billing.php
+++ wp-content/themes/custom/woocommerce/checkout/form-billing.php
@@ -26,11 +26,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="woocommerce-billing-fields">
 	<?php if ( wc_ship_to_billing_address_only() && WC()->cart->needs_shipping() ) : ?>
 
-		<h3><?php _e( 'Billing &amp; Shipping', 'woocommerce' ); ?></h3>
+	<h3><?php _e( 'Billing &amp; Shipping', 'woocommerce' ); ?></h3>
 
 	<?php else : ?>
-
-		<h3><?php _e( 'Billing details', 'woocommerce' ); ?></h3>
+		<?php $nam_cart_total = $woocommerce->cart->get_cart_total(); ?>
+		<?php if($nam_cart_total > 0): ?>
+			<h3>Billing Details</h3>
+			<?php else; ?>
+			<h3>Contact Details</h3>
+		<?php endif; ?>
 
 	<?php endif; ?>
 
@@ -38,14 +42,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="woocommerce-billing-fields__field-wrapper">
 		<?php
-			$fields = $checkout->get_checkout_fields( 'billing' );
+		$fields = $checkout->get_checkout_fields( 'billing' );
 
-			foreach ( $fields as $key => $field ) {
-				if ( isset( $field['country_field'], $fields[ $field['country_field'] ] ) ) {
-					$field['country'] = $checkout->get_value( $field['country_field'] );
-				}
-				woocommerce_form_field( $key, $field, $checkout->get_value( $key ) );
+		foreach ( $fields as $key => $field ) {
+			if ( isset( $field['country_field'], $fields[ $field['country_field'] ] ) ) {
+				$field['country'] = $checkout->get_value( $field['country_field'] );
 			}
+			woocommerce_form_field( $key, $field, $checkout->get_value( $key ) );
+		}
 		?>
 	</div>
 
@@ -53,30 +57,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 </div>
 
 <?php if ( ! is_user_logged_in() && $checkout->is_registration_enabled() ) : ?>
-	<div class="woocommerce-account-fields">
-		<?php if ( ! $checkout->is_registration_required() ) : ?>
+<div class="woocommerce-account-fields">
+	<?php if ( ! $checkout->is_registration_required() ) : ?>
 
-			<p class="form-row form-row-wide create-account">
-				<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-					<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="createaccount" <?php checked( ( true === $checkout->get_value( 'createaccount' ) || ( true === apply_filters( 'woocommerce_create_account_default_checked', false ) ) ), true ) ?> type="checkbox" name="createaccount" value="1" /> <span><?php _e( 'Create an account?', 'woocommerce' ); ?></span>
-				</label>
-			</p>
+		<p class="form-row form-row-wide create-account">
+			<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
+				<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="createaccount" <?php checked( ( true === $checkout->get_value( 'createaccount' ) || ( true === apply_filters( 'woocommerce_create_account_default_checked', false ) ) ), true ) ?> type="checkbox" name="createaccount" value="1" /> <span><?php _e( 'Create an account?', 'woocommerce' ); ?></span>
+			</label>
+		</p>
 
-		<?php endif; ?>
+	<?php endif; ?>
 
-		<?php do_action( 'woocommerce_before_checkout_registration_form', $checkout ); ?>
+	<?php do_action( 'woocommerce_before_checkout_registration_form', $checkout ); ?>
 
-		<?php if ( $checkout->get_checkout_fields( 'account' ) ) : ?>
+	<?php if ( $checkout->get_checkout_fields( 'account' ) ) : ?>
 
-			<div class="create-account">
-				<?php foreach ( $checkout->get_checkout_fields( 'account' ) as $key => $field ) : ?>
-					<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
-				<?php endforeach; ?>
-				<div class="clear"></div>
-			</div>
+		<div class="create-account">
+			<?php foreach ( $checkout->get_checkout_fields( 'account' ) as $key => $field ) : ?>
+				<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
+			<?php endforeach; ?>
+			<div class="clear"></div>
+		</div>
 
-		<?php endif; ?>
+	<?php endif; ?>
 
-		<?php do_action( 'woocommerce_after_checkout_registration_form', $checkout ); ?>
-	</div>
+	<?php do_action( 'woocommerce_after_checkout_registration_form', $checkout ); ?>
+</div>
 <?php endif; ?>
