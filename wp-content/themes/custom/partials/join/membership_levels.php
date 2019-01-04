535fc87877a398b6a4c43919745ab795ce9d38c6
Nic Schumann <nicolas_schumann@brown.edu>
2019-01-04 15:12:04 -0500

    [membership tiers] added a guard against displaying hidden membership tiers.

--- wp-content/themes/custom/partials/join/membership_levels.php
+++ wp-content/themes/custom/partials/join/membership_levels.php
@@ -17,49 +17,55 @@
 							<?php $the_query->the_post(); ?>
 							<?php
 							$id = $post->ID;
-							$product_id = get_field('managed_field_related_post', $id)[0];
-							$product = wc_get_product( $product_id );
-							$current_price = $product->get_price();
-							$sale_price = $product->get_sale_price();
-							$regular_price = $product->get_regular_price();
-							$sale_start_date = $product->get_date_on_sale_from();
-							$sale_end_date = $product->get_date_on_sale_to();
-							$is_on_sale = $product->is_on_sale();
-							// woocommerce plumbing
-							$add_to_cart_url = $product->add_to_cart_url();
-							$add_to_cart_button_text = $product->add_to_cart_url();
-							?>
-							<div class="accordion multi-collapse" data-accordion>
-								<div class="accordion-label" data-control>
-									<h4 class="accordion-title">
-										<span class="bold mr2"><?php the_title(); ?></span> <span class="membership-level-price">$<?php echo $current_price; ?></span>
-									</h4>
-									<span class="icon" data-icon="”"></span>
-								</div>
-								<div class="accordion-body" data-content>
-									<div class="accordion-content-inner">
-										<div class="wysiwyg">
-											<?php the_field('membership_level_description'); ?>
-										</div>
-										<?php if( !is_user_logged_in() && NAM_Membership::has_membership_in_cart() ): ?>
-										<div class="bg-error p1 mt2">
-											<h5 class="bold m0">
-												You already have a membership in your <a href="/cart" class="underline">cart.</a> Memberships are limited to one per customer.
-											</h5>
-										</div>
-										<?php else: ?>
-											<div class="accordion-link membership-link-button">
-												<a href="<?php echo $add_to_cart_url; ?>" class="">
-													Purchase New Membership
-												</a>
-												<a href="/renew-your-membership" class="ml1">
-													Renew Your Membership
-												</a>
-											</div>
-										<?php endif; ?>
-									</div>
-								</div>
-							</div>
+                            $product_is_hidden = get_field( 'hidden_membership_tier', $id );
+                            ?>
+
+                            <?php if ( !$product_is_hidden ) : ?>
+                                <?php
+    							$product_id = get_field('managed_field_related_post', $id)[0];
+    							$product = wc_get_product( $product_id );
+    							$current_price = $product->get_price();
+    							$sale_price = $product->get_sale_price();
+    							$regular_price = $product->get_regular_price();
+    							$sale_start_date = $product->get_date_on_sale_from();
+    							$sale_end_date = $product->get_date_on_sale_to();
+    							$is_on_sale = $product->is_on_sale();
+    							// woocommerce plumbing
+    							$add_to_cart_url = $product->add_to_cart_url();
+    							$add_to_cart_button_text = $product->add_to_cart_url();
+    							?>
+    							<div class="accordion multi-collapse" data-accordion>
+    								<div class="accordion-label" data-control>
+    									<h4 class="accordion-title">
+    										<span class="bold mr2"><?php the_title(); ?></span> <span class="membership-level-price">$<?php echo $current_price; ?></span>
+    									</h4>
+    									<span class="icon" data-icon="”"></span>
+    								</div>
+    								<div class="accordion-body" data-content>
+    									<div class="accordion-content-inner">
+    										<div class="wysiwyg">
+    											<?php the_field('membership_level_description'); ?>
+    										</div>
+    										<?php if( !is_user_logged_in() && NAM_Membership::has_membership_in_cart() ): ?>
+    										<div class="bg-error p1 mt2">
+    											<h5 class="bold m0">
+    												You already have a membership in your <a href="/cart" class="underline">cart.</a> Memberships are limited to one per customer.
+    											</h5>
+    										</div>
+    										<?php else: ?>
+    											<div class="accordion-link membership-link-button">
+    												<a href="<?php echo $add_to_cart_url; ?>" class="">
+    													Purchase New Membership
+    												</a>
+    												<a href="/renew-your-membership" class="ml1">
+    													Renew Your Membership
+    												</a>
+    											</div>
+    										<?php endif; ?>
+    									</div>
+    								</div>
+    							</div>
+                        <?php endif; ?>
 						<?php endwhile; ?>
 						<?php wp_reset_postdata(); ?>
 					</div>
