9a83c502bc5dca332bddd1c11930b7a1f5e2392b
Nic Schumann <nicolas_schumann@brown.edu>
2019-02-28 11:13:04 -0500

    [subscripions & errors] adds error icon and support for subscription page.

--- wp-content/themes/custom/js/admin-order-links.js
+++ wp-content/themes/custom/js/admin-order-links.js
@@ -14,7 +14,7 @@ function adminOrderLinks() {
             (( window.location.port !== '80' ) ? (':' + window.location.port) : '') +
             '/wp-json/nam/v1/parent/';
 
-        if ( $( document.body ).hasClass('post-type-shop_order') ) {
+        if ( $( document.body ).hasClass('post-type-shop_order') || $( document.body ).hasClass('post-type-shop_subscription') ) {
 
             const items = $('#order_line_items .item');
 
@@ -22,9 +22,10 @@ function adminOrderLinks() {
 
                 const item = $( this );
                 const a = item.find('a');
-                const replacement = $('<span>').text( a.text() );
+                const replacement = $('<span>').addClass('order-link-placeholder').text( a.text() );
+                const icon = $('<img>').addClass('order-admin-icon').addClass('order-loading-icon').attr('src', '/wp-content/themes/custom/images/loading.gif');
 
-                replacement.prepend( $('<img>').addClass('order-loading-icon').attr('src', '/wp-content/themes/custom/images/loading.gif') );
+                replacement.prepend( icon );
                 a.replaceWith( replacement );
 
                 const href = a.attr('href');
@@ -36,13 +37,15 @@ function adminOrderLinks() {
 
                         let new_href = '/wp-admin/post.php?post=' + data.parent_id + '&action=edit';
                         let final_replacement = $('<a>').attr('href', new_href ).text( replacement.text() );
-                        replacement.replaceWith( final_replacement );
+
+                        item.find('.order-link-placeholder').replaceWith( final_replacement );
 
                         if ( data.warning ) { console.error( data.message ); }
 
                     } else {
 
                         console.error( data.message );
+                        icon.removeClass('order-loading-icon').addClass('order-failed-icon').attr('src', '/wp-content/themes/custom/images/close.png' );
 
                     }
 
