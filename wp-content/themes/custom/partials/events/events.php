e1f2005aafb6717f535ceabce31ae035c22cd45f
Greg Nemes <mail@gregnemes.com>
2019-09-20 17:16:57 -0400

    added flag for private events

--- wp-content/themes/custom/partials/events/events.php
+++ wp-content/themes/custom/partials/events/events.php
@@ -4,17 +4,19 @@
 	<div class="container-fluid container-fluid-stretch">
 		<div class="row">
 			<?php foreach ( $posts as $post ) : ?>
-				<div class="col-md-4 col-xl-3 filter-target filter-event <?php NAM_Helpers::card_terms('events-categories'); ?>" data-date="<?php $event_date = get_field('event_date'); $event_date = strtotime($event_date); $event_date = date('m/d/Y', $event_date); echo $event_date; ?>">
-					<?php
-					setup_postdata( $post );
+				<?php setup_postdata( $post ); ?>
+				<?php if( has_term( 'private', 'events-categories' ) === false ) : ?>
+					<div class="col-md-4 col-xl-3 filter-target filter-event <?php NAM_Helpers::card_terms('events-categories'); ?>" data-date="<?php $event_date = get_field('event_date'); $event_date = strtotime($event_date); $event_date = date('m/d/Y', $event_date); echo $event_date; ?>">
+						<?php
 					$card_layout = 'text_bottom'; //'text_right', 'text_bottom', 'text_top'
 					$card_size = 'medium'; //'wide', 'medium', 'small', 'menu'
 					$card_type = 'event';  //'event', 'class', 'product', 'news' 
 					NAM_Helpers::card( $card_layout, $card_size, $card_type, null, null, null, null ); ?>
 				</div>
-			<?php endforeach; ?>
-			<?php wp_reset_postdata(); ?>
-		</div>
+			<?php endif; ?>
+		<?php endforeach; ?>
+		<?php wp_reset_postdata(); ?>
 	</div>
+</div>
 </section>
 
